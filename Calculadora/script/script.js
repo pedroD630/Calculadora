2eff3a63c54441c57bee92d6d3be3b62a73ab216
Pedro Silva <87133537+pedroD630@users.noreply.github.com>
2022-06-06 10:37:30 -0300

    Update script.js

--- Calculadora/script/script.js
+++ Calculadora/script/script.js
@@ -1,20 +1,16 @@
-// NOTE: 
-// This is the final source code file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
-
-const lculateulate = (n1, operator, n2) => {
-  let result = ''
-  if (operator === 'add') {
-    result = parseFloat(n1) + parseFloat(n2)
-  } else if (operator === 'subtract') {
-    result = parseFloat(n1) - parseFloat(n2)
-  } else if (operator === 'multiply') {
-    result = parseFloat(n1) * parseFloat(n2)
-  } else if (operator === 'divide') {
-    result = parseFloat(n1) / parseFloat(n2)
-  }
-
-  return result
+function insert(num){
+  document.form.textview.value = document.form.textview.value + num;
 }
-
-const calculator = document.querySelector('.calculator')
-const display = calculator.querySelector('.calculator__display')
+function equal(){
+  exp = document.form.textview.value;
+  if(exp){
+     document.form.textview.value = eval(exp);
+}
+}
+function c(){
+  document.form.textview.value = "";
+}
+function back(){
+  var exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0, exp.length-1);
+}
